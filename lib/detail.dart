d1748f6a96662a48fc15f5196c3903d806f8c6ac
jehan <asmjehan@gmail.com>
2021-06-04 15:19:42 +0600

    data passed to 2nd page

--- lib/detail.dart
+++ lib/detail.dart
@@ -1,29 +1,31 @@
 import 'package:flutter/material.dart';
 
 class Detail extends StatefulWidget {
-  Detail(post);
-
-  @override
-  _DetailState createState() => _DetailState();
+  var data;
+  Detail(this.data);
+  _DetailState createState() => _DetailState(this.data);
 }
 
 class _DetailState extends State<Detail> {
+  var data;
+  _DetailState(this.data);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Detail"),
       ),
-      body: Column(
-        children: [
-          Text("detail page"),
-          RaisedButton(
-            onPressed: () {
-              Navigator.pop(context);
-            },
-            child: Text("Back to Dashboard"),
-          ),
-        ],
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text("id:      ${data['id']}"),
+            Text("title:   ${data['title']}"),
+            Divider(),
+            Text("description:    ${data['body']}"),
+            Divider(),
+          ],
+        ),
       ),
     );
   }
